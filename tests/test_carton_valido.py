c8fe60f2b5af6ee0f85a14b512cfb3b6969c98ea
Eugenio Bergia <eugeniobergia1@gmail.com>
2020-04-16 12:23:46 -0300

    Comento el codigo de los tests

--- tests/test_carton_valido.py
+++ tests/test_carton_valido.py
@@ -1,5 +1,6 @@
 from src.bingo import carton
 
+# Cuenta la cantidad de celdas ocupadas, funcion axuiliar para los tests
 def contar_celdas_ocupadas():
     mi_carton = carton()
     contador = 0
@@ -8,14 +9,18 @@ def contar_celdas_ocupadas():
             contador += celda
     return contador
 
+# Guarda la cantidad de celdas ocupadas
 cant_celdas_ocupadas = contar_celdas_ocupadas()
 
+# Verifica que la cantidad de celdas ocupadas no sea menor a 15
 def test_no_menos_de_15():
     assert cant_celdas_ocupadas >= 15
 
+# Verifica que la cantidad de celdas ocupadas no sea mayor a 15
 def test_no_mas_de_15():
     assert cant_celdas_ocupadas <= 15
 
+# Verifica que no haya columnas vacias
 def test_sin_colums_vacias():
     mi_carton = carton()
     for i in range(9):
