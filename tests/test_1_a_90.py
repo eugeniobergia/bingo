bc010e1e278ffd704d9d90f587b1f4a557754c1b
Eugenio Bergia <eugeniobergia1@gmail.com>
2020-06-03 17:48:24 -0300

    Borrados archivos innecesarios

--- tests/test_1_a_90.py
+++
@@ -1,6 +0,0 @@
-from src import bingo
-
-mi_carton = bingo.carton()
-
-def test_celdas_ocupadas_1_a_90():
-    assert bingo.celdas_ocupadas_1_a_90(mi_carton)
--- tests/test_mayores_abajo.py
+++
@@ -1,6 +0,0 @@
-from src import bingo
-
-mi_carton = bingo.carton()
-
-def test_mayores_abajo():
-    assert bingo.mayores_abajo(mi_carton)
